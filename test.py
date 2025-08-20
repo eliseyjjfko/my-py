664d75b43c952d2e2a8a822fbc2baf4207e80600
eliseyjjfko <eliseyfilin.ef@gmail.com>
2025-08-20 20:42:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 Print("Hello world")
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное"
+            }
+word = input("Введите непонятное слово (большими буквами!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("ничего не нашлось")
